a0d9092f7aa571b0ef22ac7be8524fab764f8635
Kim Litjens <Kimlitjens4@gmail.com>
2021-03-06 17:01:25 +0100

    add & styled card component

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import MovieCard from './movieCard.js'
 import { apiKey } from './apiKey'
 
 export default function SearchMovies() {
@@ -34,13 +35,7 @@ export default function SearchMovies() {
             </form>
             <div className="card-list">
                 {movies.filter(movie => movie.poster_path).map(movie => (
-                    <div className="card" key={movie.id}>
-                        <img className="card--image"
-                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                            alt={movie.title + ' poster'}
-                        />
-
-                    </div>
+                    <MovieCard movie={movie} key={movie.id} />
                 ))}
             </div>
         </>
