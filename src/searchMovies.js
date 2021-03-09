0197bf6784281f078cfe88e2a6492c6cf1e0281b
Kim Litjens <Kimlitjens4@gmail.com>
2021-03-09 15:21:33 +0100

    added sortfunction & changed some text color

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import MovieCard from './movieCard.js'
 import { apiKey } from './apiKey'
 
@@ -8,6 +8,8 @@ export default function SearchMovies() {
     const [query, setQuery] = useState('');
     //create the state for movies, and update that state appropriate
     const [movies, setMovies] = useState([]);
+    const [sortBy, setSortBy] = useState();
+
 
     const searchMovies = async (e) => {
         e.preventDefault();
@@ -23,6 +25,15 @@ export default function SearchMovies() {
         }
     }
 
+    useEffect(() => {
+
+        const sortedMovies = sortBy === "High-Low" ? [...movies].sort((a, b) => b.vote_average - a.vote_average)
+            : sortBy === "Low-High" ? [...movies].sort((a, b) => a.vote_average - b.vote_average)
+                : sortBy === "ReleaseDate" ? [...movies].sort((a, b) => new Date(b.release_date) - new Date(a.release_date)) : [...movies].sort((a, b) => a.title.localeCompare(b.title))
+        setMovies(sortedMovies)
+        console.log(movies[0])
+    }, [sortBy])
+
     return (
         <>
             <form className="form" onSubmit={searchMovies}>
@@ -33,6 +44,18 @@ export default function SearchMovies() {
                 />
                 <button className="button" type="submit">Search</button>
             </form>
+            <div className="sortMovies">
+                <form id="sortForm">
+                    Sort by:
+            <select id="sortSelect" onChange={(e) => setSortBy(e.target.value)}>
+                        <option >---</option>
+                        <option value="High-Low">High-Low</option>
+                        <option value="Low-High">Low-High</option>
+                        <option value="Title">Title</option>
+                        <option value="ReleaseDate">Release Date</option>
+                    </select>
+                </form>
+            </div>
             <div className="card-list">
                 {movies.filter(movie => movie.poster_path).map(movie => (
                     <MovieCard movie={movie} key={movie.id} />
