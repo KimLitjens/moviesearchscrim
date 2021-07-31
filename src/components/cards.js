f9bf19e4ad76fbde5df70031258160baba88dccd
Kim Litjens <Kimlitjens4@gmail.com>
2021-07-31 19:05:23 +0200

    add responsive to cards list

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function Cards({ children, ...restProps }) {
-    return <div {...restProps} className="grid grid-flow-rows grid-cols-3 grid-rows-3 gap-4 mt-16 ">{children}</div>
+    return <div {...restProps} className="grid grid-flow-rows md:grid-cols-3 grid-rows-3 gap-4 mt-16">{children}</div>
 }
