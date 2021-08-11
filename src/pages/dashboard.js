e8c456ada5f8fea055a0aec1861231c4cf86870e
Kim Litjens <Kimlitjens4@gmail.com>
2021-08-11 18:09:58 +0200

    add genre filter

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { apiKey } from '../apiKey'
 import { HeaderContainer, MovieCardContainer, } from '../containers'
 import { Search, Filter, Cards } from '../components'
+import { allGenres } from '../allGenres'
 
 export default function Dashboard() {
     const [query, setQuery] = useState('');
@@ -9,6 +10,8 @@ export default function Dashboard() {
     const [filterdMoviesTvShow, setFilterdMoviesTvShow] = useState([])
     const [sortBy, setSortBy] = useState();
     const [channelAt, setChannelAt] = useState();
+    const [usedGenres, setUsedGenres] = useState([])
+    const [selectedGenreId, setSelectedGenreId] = useState()
 
 
     const searchMovies = async (e) => {
@@ -29,18 +32,20 @@ export default function Dashboard() {
             console.error(err);
         }
 
-        const filterBy = document.getElementById("filterBy");
-        filterBy.selectedIndex = 0;
+        const filterByType = document.getElementById("filterByType");
+        filterByType.selectedIndex = 0;
         const sortBy = document.getElementById("sortBy");
         sortBy.selectedIndex = 0;
+        const filterByGenre = document.getElementById("filterByGenre");
+        filterByGenre.selectedIndex = 0;
     }
 
     useEffect(() => {
-        const sortedMovies = sortBy === "High-Low" ? [...moviesTvShows].sort((a, b) => b.vote_average - a.vote_average)
-            : sortBy === "Low-High" ? [...moviesTvShows].sort((a, b) => a.vote_average - b.vote_average)
-                : sortBy === "NewestFirst" ? [...moviesTvShows].sort((a, b) => new Date(b.release_date || b.first_air_date) - new Date(a.release_date || a.first_air_date))
-                    : sortBy === "OldestFirst" ? [...moviesTvShows].sort((a, b) => new Date(a.release_date || a.first_air_date) - new Date(b.release_date || b.first_air_date))
-                        : [...moviesTvShows].sort((a, b) => (a.title || a.name).localeCompare(b.title || b.name))
+        const sortedMovies = sortBy === "High-Low" ? [...filterdMoviesTvShow].sort((a, b) => b.vote_average - a.vote_average)
+            : sortBy === "Low-High" ? [...filterdMoviesTvShow].sort((a, b) => a.vote_average - b.vote_average)
+                : sortBy === "NewestFirst" ? [...filterdMoviesTvShow].sort((a, b) => new Date(b.release_date || b.first_air_date) - new Date(a.release_date || a.first_air_date))
+                    : sortBy === "OldestFirst" ? [...filterdMoviesTvShow].sort((a, b) => new Date(a.release_date || a.first_air_date) - new Date(b.release_date || b.first_air_date))
+                        : [...filterdMoviesTvShow].sort((a, b) => (a.title || a.name).localeCompare(b.title || b.name))
         setFilterdMoviesTvShow(sortedMovies)
     }, [sortBy])
 
@@ -50,6 +55,18 @@ export default function Dashboard() {
                 : [...moviesTvShows]
         setFilterdMoviesTvShow(selectedChannel)
     }, [channelAt])
+
+    useEffect(() => {
+        const filterdByGenre = [...moviesTvShows].filter(i => i.genre_ids.includes(parseInt(selectedGenreId)))
+        setFilterdMoviesTvShow(filterdByGenre)
+    }, [selectedGenreId])
+
+    useEffect(() => {
+        let genreIds = []
+        moviesTvShows.map(e => genreIds = genreIds.concat(e.genre_ids))
+        const usedGenres = allGenres.filter(g => genreIds.includes(g.id))
+        setUsedGenres(usedGenres)
+    }, [filterdMoviesTvShow])
 
     return (
         <div className="container">
@@ -70,7 +87,7 @@ export default function Dashboard() {
                 <Filter.Form>
                     <Filter.Label >Sort By:</Filter.Label>
                     <Filter.Select id="sortBy" onChange={(e) => setSortBy(e.target.value)}>
-                        <option selected>---</option>
+                        <option value="">---</option>
                         <option value="High-Low">High-Low</option>
                         <option value="Low-High">Low-High</option>
                         <option value="Title">Title</option>
@@ -80,13 +97,22 @@ export default function Dashboard() {
                 </Filter.Form>
                 {/* Filter Search  */}
                 <Filter.Form >
-                    <Filter.Label>Filter:</Filter.Label>
-                    <Filter.Select id="filterBy"
+                    <Filter.Label>Type:</Filter.Label>
+                    <Filter.Select id="filterByType"
                         onChange={(e) => setChannelAt(e.target.value)}
                     >
                         <option value="">---</option>
                         <option value="movies">Movies</option>
                         <option value="tvShows">TV Shows</option>
+                    </Filter.Select>
+                </Filter.Form>
+                {/* Filter Genre  */}
+                <Filter.Form>
+                    <Filter.Label>Genre:</Filter.Label>
+                    <Filter.Select id="filterByGenre"
+                        onChange={(e) => setSelectedGenreId(e.target.value)}>
+                        <option value="">---</option>
+                        {usedGenres.map(g => <option value={g.id}>{g.name}</option>)}
                     </Filter.Select>
                 </Filter.Form>
             </Filter>
